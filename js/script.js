ca4c3b637be4ee112d7a191f42d5d42e88165ff3
xytz <1402755128@qq.com>
2016-09-09 11:12:50 +0800

    first
    first

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by 欢迎使用 on 2016-07-24.
+ */
+$(function(){
+    //实现导航跳转效果
+    $(".nav").singlePageNav({
+        offset:70
+    });
+    //小屏幕点击关闭导航菜单
+    $(".navbar-collapse a").click(function(){
+        $(".navbar-collapse").collapse("hide");
+    });
+    //使用wow.min.js
+    new WOW().init();
+
+})
